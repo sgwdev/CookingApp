67ea84a7713f9f8d1c576268eb3a37df7b640200
sgwdev <kichi.pc@gmail.com>
2019-08-20 18:47:12 +0900

    Add sample data for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ingredients = Ingredient.create([{ name: 'Chocolate'}, { name: 'Sugar'}, { name: 'Salt'}, { name: 'Milk'}, { name: 'Flour'}, { name: 'Egg' }, { name: 'Cinnamon'}, { name: 'Butter'}])
+unites = Unit.create([{ name: 'Gram', symbol: 'g' }, { name: 'Milliliter', symbol: 'ml' }])
